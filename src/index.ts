918bdbd6dfc9340ccc03695ee1e0ee847af50b60
arredondode <104038069+arredondode@users.noreply.github.com>
2022-05-06 14:13:20 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+//CALCULAR EL ÁREA DE UN RECTÁNGULO//
+let altura: number = Number(prompt("Ingrese la altura del rectángulo"));
+let base: number = Number(prompt("Ingrese la base del rectángulo "));
+console.log("El área de su rectangulo es: " + base * altura);
